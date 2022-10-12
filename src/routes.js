a3ea5a8496d783ccc7f87a6003b4b8cd552c5002
name <name@email.com>
2022-10-12 17:52:27 +0200

    Back to version worked ...

--- src/routes.js
+++ src/routes.js
@@ -5,62 +5,49 @@ const {
 } = Apify;
 const { applyFunction, saveScreenshot } = require('./utils');
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
 
 async function autoScroll(page) {
 
-    await sleep(1000)
+    let lstJob = await page.evaluate(() => { return document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed') })
+    console.log("Nb jobs loaded :", lstJob.length)
+    console.log("Scrolling ...")
 
-    for (let i = 0; i < 20; i++) {
+    await page.evaluate(async () => {
 
-        let lstJob = Array.from(await page.evaluate(() => { return document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed') }))
+        function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
 
-        console.log("Lst jobs : ", lstJob)
-        console.log("Nb jobs loaded :", lstJob.length)
-        console.log("Scrolling ...")
+        let lastHeight = 0
 
-        await page.evaluate(async () => {
 
-            function sleep(ms) {
-                return new Promise(resolve => setTimeout(resolve, ms));
+        async function scrollThat() {
+
+            // console.log("scrollThat 1")
+            await sleep(500)
+            // console.log("scrollThat 2")
+            var totalHeight = 0;
+            var distance = 20000;
+            let elementScrolled = document.querySelector('.gws-plugins-horizon-jobs__tl-no-filters')
+            var scrollHeight = elementScrolled.scrollHeight
+            document.querySelector('.zxU94d.gws-plugins-horizon-jobs__tl-lvc').scrollBy(0, distance);
+            totalHeight += distance;
+
+            await sleep(1500)
+            // console.log("scrollThat 3", lastHeight, elementScrolled.clientHeight)
+            if (lastHeight < elementScrolled.clientHeight) {
+                lastHeight = elementScrolled.clientHeight
+                return scrollThat()
+            } else {
+                return
             }
+        }
 
-            let lastHeight = 0
+        await scrollThat()
+    }, { timeout: 0 });
 
-
-            async function scrollThat() {
-
-                // console.log("scrollThat 1")
-                await sleep(500)
-                // console.log("scrollThat 2")
-                var totalHeight = 0;
-                var distance = 20000;
-                let elementScrolled = document.querySelector('.gws-plugins-horizon-jobs__tl-no-filters')
-                var scrollHeight = elementScrolled.scrollHeight
-                document.querySelector('.zxU94d.gws-plugins-horizon-jobs__tl-lvc').scrollBy(0, distance);
-                totalHeight += distance;
-
-                await sleep(1500)
-                console.log("scrollThat 3", lastHeight, elementScrolled.clientHeight)
-                // if (lastHeight < elementScrolled.clientHeight) {
-                //     lastHeight = elementScrolled.clientHeight
-                // return scrollThat()
-                // } else {
-                //     return
-                // }
-            }
-
-            await scrollThat()
-        });
-
-        let lstJob2 = Array.from(await page.evaluate(() => { return document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed') }))
-        console.log("Nb jobs loaded 2 :", lstJob2.length)
-
-        // if (lstJob2.length === lstJob.length) break;
-    }
-
+    let lstJob2 = await page.evaluate(() => { return document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed') })
+    console.log("Nb jobs loaded 2 :", lstJob2.length)
 
 }
 
@@ -70,105 +57,75 @@ exports.SEARCH_PAGE = async (countryCode, page, request, query, requestQueue, ma
     let { savedItems, pageNumber } = request.userData;
     const { hostname } = request.userData;
 
-    await page.waitForSelector('div.KzzVYe')
-    await page.waitForSelector('.gws-plugins-horizon-jobs__li-ed')
+    await page.waitForSelector('div.KzzVYe');
 
-    let firstJob = await page.evaluate(() => { return document.querySelector('.gws-plugins-horizon-jobs__li-ed') })
-    console.log("First job :", firstJob)
-
-    await autoScroll(page)
+    await autoScroll(page);
 
     let lstJob = await page.evaluate(() => { return document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed') })
     let nbResults = lstJob.length
 
-    // log.info(`Found ${resultsLength} products on the page.`); 
+    // log.info(`Found ${resultsLength} products on the page.`);
     // eslint-disable-next-line no-shadow
+    const data = await page.evaluate(
+        async (countryCode, maxPostCount, query, savedItems) => {
 
-    console.log("Scroll ended ...")
-    let data = []
+            function sleep(ms) {
+                return new Promise(resolve => setTimeout(resolve, ms));
+            }
 
-    for (let i = 0; i < 20; i++) {
+            let data = []
+            let lstJob = Array.from(document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed'))
+            for (let jobElement of lstJob) {
 
-        console.log("Offset  ...", i)
+                jobElement.querySelector('.Fol1qc').click()
 
-        const lstData = await page.evaluate(
-            async (offset, countryCode, maxPostCount, query, savedItems) => {
+                await sleep(1000)
 
-                function sleep(ms) {
-                    return new Promise(resolve => setTimeout(resolve, ms));
+                // Wait ? 0.8 sec ?
+                const jobContentElement = document.querySelector('.whazf.bD1FPe .pE8vnd.avtvi')
+                
+                if (!jobContentElement.querySelector('.sH3zFd .KLsYvd')) continue
+
+                const title = jobContentElement.querySelector('.sH3zFd .KLsYvd').innerText
+
+                let content = ''
+                if (jobContentElement.querySelector('.HBvzbc'))
+                    content = jobContentElement.querySelector('.HBvzbc').innerText
+                else
+                    content = jobContentElement.querySelector('.JvOW3e')?.innerText
+
+                if (content) {
+                    content = content.replace(/\s+/g, ' ')
                 }
 
-                let data = []
-                let lstJob = Array.from(document.querySelectorAll('.gws-plugins-horizon-jobs__li-ed'))
+                // console.log("Job", title)
 
-                try {
+                const elemEmployerLocation = jobContentElement.querySelector('.tJ9zfc')
+                const elemsDiv = elemEmployerLocation.querySelectorAll(':scope > div')
 
-                    let idx = 0
-                    for (let jobElement of lstJob) {
-    
-                        idx++
-                        if (idx <= offset) continue
-    
-                        if (jobElement.querySelector('.Fol1qc'))
-                        jobElement.querySelector('.Fol1qc').click()
-    
-                        await sleep(1500)
-    
-                        // Wait ? 0.8 sec ?
-                        const jobContentElement = document.querySelector('.whazf.bD1FPe .pE8vnd.avtvi')
-    
-                        if (!jobContentElement.querySelector('.sH3zFd .KLsYvd')) continue
-    
-                        const title = jobContentElement.querySelector('.sH3zFd .KLsYvd').innerText
-    
-                        let content = ''
-                        if (jobContentElement.querySelector('.HBvzbc'))
-                            content = jobContentElement.querySelector('.HBvzbc').innerText
-                        else
-                            content = jobContentElement.querySelector('.JvOW3e')?.innerText
-    
-                        if (content) {
-                            content = content.replace(/\s+/g, ' ')
-                        }
-    
-                        // console.log("Job", title)
-    
-                        const elemEmployerLocation = jobContentElement.querySelector('.tJ9zfc')
-                        const elemsDiv = elemEmployerLocation.querySelectorAll(':scope > div')
-    
-                        // console.log("Employer location : ", elemEmployerLocation)
-    
-                        const employer = elemsDiv[0]?.innerText
-                        const location = elemsDiv[1]?.innerText
-    
-                        // Get infos from job :
-                        data.push({
-                            countryCode,
-                            query,
-                            title,
-                            content,
-                            employer,
-                            location,
-                        })
-    
-                        if ((idx - offset) >= 10) break
-                    }
-                    
-                } catch (err) {}
-                
+                // console.log("Employer location : ", elemEmployerLocation)
 
-                return data;
-            },
-            i * 10,
-            countryCode,
-            maxPostCount,
-            query,
-            savedItems,
-        );
+                const employer = elemsDiv[0]?.innerText
+                const location = elemsDiv[1]?.innerText
 
-        data.push(...lstData)
-        console.log("Nb data :", data.length, "(+", lstData.length, ")")
-    }
+                // Get infos from job :
+                data.push({
+                    countryCode,
+                    query,
+                    title,
+                    content,
+                    employer,
+                    location,
+                })
+            }
+
+            return data;
+        },
+        countryCode,
+        maxPostCount,
+        query,
+        savedItems,
+    );
 
     await saveScreenshot(page)
 
